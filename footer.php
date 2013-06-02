b9967b1ba63b5a5b78e9758132875df03e2b37d0
Jake Spurlock <whyisjake@gmail.com>
2013-06-01 23:05:29 -0700

    More of the home page. Good for the night.

--- footer.php
+++ footer.php
@@ -26,7 +26,12 @@
 				</ul>
 			</nav>
 			<p><a href="mailto:info@flyinggoatcoffee.com">info@flyinggoatcoffee.com</a> &ndash; 707.433.3599</p>
-			<p class="meta">&copy; <?php date('Y'); ?> Flying Goat Coffee <a href="#">MY ACCOUNT</a> &bull; <a href="#">TERMS + CONDITIONS</a> &bull; <a href=#>PRIVACY POLICY</a> &bull; <a href="#">ORDERING + SHIPPING</a> <span class="input-append"><input class="input-small" id="appendedInputButtons" type="text"> <button class="btn" type="button">Search</button></span></p>
+			<div class="meta">&copy; <?php date('Y'); ?> Flying Goat Coffee <a href="#">MY ACCOUNT</a> &bull; <a href="#">TERMS + CONDITIONS</a> &bull; <a href=#>PRIVACY POLICY</a> &bull; <a href="#">ORDERING + SHIPPING</a> 
+				<form class="form-inline">
+					<input class="input-small" type="email" placeholder="Subscribe to newsletter" id="appendedInputButtons" type="text">
+					<button class="btn" type="button">Subscribe</button>
+				</form>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
