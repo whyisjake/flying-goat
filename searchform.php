2b6805cda70f7b0de644b4f479b0adbf4968a4d4
Jake Spurlock <whyisjake@gmail.com>
2013-06-25 15:00:12 -0600

    Better Bootstrap search form.

--- searchform.php
+++ searchform.php
@@ -5,8 +5,7 @@
  * @package flying-goat
  */
 ?>
-	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="screen-reader-text"><?php _ex( 'Search', 'assistive text', 'flying_goat' ); ?></label>
+	<form method="get" id="searchform form form-inline" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'flying_goat' ); ?>" />
-		<input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'flying_goat' ); ?>" />
+		<input type="submit" class="btn submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'flying_goat' ); ?>" />
 	</form>
