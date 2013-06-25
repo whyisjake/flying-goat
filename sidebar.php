e1aac4213946afc26816bcbb43aec431f0ddb362
Jake Spurlock <whyisjake@gmail.com>
2013-06-25 17:12:09 -0600

    Stlyes for sidebar navigation

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,33 @@
 <?php
 /**
- * The Sidebar...
+ * The Sidebar containing the main widget areas.
  *
  * @package flying-goat
  */
 ?>
 	<div id="secondary" class="widget-area span6 sidebar" role="complementary">
-		
-		<?php
+		<?php do_action( 'before_sidebar' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			$args = array(
-				'posts_per_page'	=> 30,
-				'no_found_rows'		=> true,
-				'post_type'			=> 'product',
-			);
-			
-			$proj_query = new WP_Query( $args );
-			echo '<ul>';
-			while ( $proj_query->have_posts() ) : $proj_query->the_post();
-				echo '<li><a href="'.get_permalink().'"><h4>' . get_the_title() . '</h4></a><li>';
-			endwhile;
-			echo '</ul>';
+			<aside id="search" class="widget widget_search">
+				<?php get_search_form(); ?>
+			</aside>
 
-			// Reset Post Data
-			wp_reset_postdata();
+			<aside id="archives" class="widget">
+				<h1 class="widget-title"><?php _e( 'Archives', 'flying_goat' ); ?></h1>
+				<ul>
+					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+				</ul>
+			</aside>
 
-		?>
+			<aside id="meta" class="widget">
+				<h1 class="widget-title"><?php _e( 'Meta', 'flying_goat' ); ?></h1>
+				<ul>
+					<?php wp_register(); ?>
+					<li><?php wp_loginout(); ?></li>
+					<?php wp_meta(); ?>
+				</ul>
+			</aside>
 
-		
+		<?php endif; // end sidebar widget area ?>
 	</div><!-- #secondary -->
