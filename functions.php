ce18fe1e750f21aa8ab27dc790c240443a105be9
Jake Spurlock <whyisjake@gmail.com>
2013-06-27 17:41:53 -0600

    Drop the sidebar headers down to h3

--- functions.php
+++ functions.php
@@ -96,8 +96,8 @@ function flying_goat_widgets_init() {
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'flying_goat_widgets_init' );
@@ -108,8 +108,8 @@ function flying_goat_widgets_init1() {
 		'id'            => 'sidebar-commerce',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'flying_goat_widgets_init1' );
