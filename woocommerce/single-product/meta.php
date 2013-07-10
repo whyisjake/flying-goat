17a6136fa51b28be74c7ec32066b2deb949ec2f7
Jake Spurlock <whyisjake@gmail.com>
2013-07-10 08:50:29 -0700

    Dropping the periods from the tags/categories.

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -21,12 +21,12 @@ global $post, $product;
 
 	<?php
 		$size = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
-		echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $size, 'woocommerce' ) . ' ', '.</span>' );
+		echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $size, 'woocommerce' ) . ' ', '</span>' );
 	?>
 
 	<?php
 		$size = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
-		echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $size, 'woocommerce' ) . ' ', '.</span>' );
+		echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $size, 'woocommerce' ) . ' ', '</span>' );
 	?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
