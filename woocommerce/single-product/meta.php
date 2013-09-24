bb9df1368ef17ae34995a9437e085489957a90b9
Jake Spurlock <whyisjake@gmail.com>
2013-09-24 15:44:45 -0700

    Divs around the cats/tags/sku.

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -14,20 +14,23 @@ global $post, $product;
 <div class="product_meta">
 
 	<?php do_action( 'woocommerce_product_meta_start' ); ?>
-
+	<div>
 	<?php if ( $product->is_type( array( 'simple', 'variable' ) ) && get_option( 'woocommerce_enable_sku' ) == 'yes' && $product->get_sku() ) : ?>
 		<span itemprop="productID" class="sku_wrapper"><?php _e( 'SKU:', 'woocommerce' ); ?> <span class="sku"><?php echo $product->get_sku(); ?></span>.</span>
 	<?php endif; ?>
-
+	</div>
+	<div>
 	<?php
 		$size = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
 		echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $size, 'woocommerce' ) . ' ', '</span>' );
 	?>
-
+	</div>
+	<div>
 	<?php
 		$size = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 		echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $size, 'woocommerce' ) . ' ', '</span>' );
 	?>
+	</div>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
