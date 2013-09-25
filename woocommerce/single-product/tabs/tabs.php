5c169c2bd6d72733f127bf68dbc92c0f454ae06d
Jake Spurlock <whyisjake@gmail.com>
2013-09-25 09:22:40 -0700

    Drop the product tabs.

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -15,26 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * Each tab is an array containing title, callback and priority.
  * @see woocommerce_default_product_tabs()
  */
-$tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
-if ( ! empty( $tabs ) ) : ?>
+echo '<hr>';
 
-	<div class="woocommerce-tabs">
-		<ul class="nav nav-tabs tabs">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
-
-				<li class="<?php echo $key ?>_tab">
-					<a href="#tab-<?php echo $key ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', $tab['title'], $key ) ?></a>
-				</li>
-
-			<?php endforeach; ?>
-		</ul>
-		<?php foreach ( $tabs as $key => $tab ) : ?>
-			<div class="panel entry-content" id="tab-<?php echo $key ?>" class="<?php ( $tab['priority'] == 10 ) ? 'active' : '' ; ?>">
-				<?php call_user_func( $tab['callback'], $key, $tab ) ?>
-			</div>
-
-		<?php endforeach; ?>
-	</div>
-
-<?php endif; ?>
+the_content();
