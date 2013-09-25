aa753abb3a9c1471e2f72c03b971430cbbc61ae9
Jake Spurlock <whyisjake@gmail.com>
2013-09-25 08:12:23 -0700

    Fix?

--- searchform.php
+++ searchform.php
@@ -7,9 +7,7 @@
 ?>
 	<form method="get" id="searchform form form-inline" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<div class="row">
-			<div class="input-append">
-				<input type="search" class="span2" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'flying_goat' ); ?>" />
-				<input type="submit" class="btn submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'flying_goat' ); ?>" />
-			</div>
+			<input type="search" class="span2" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'flying_goat' ); ?>" />
+			<input type="submit" class="btn submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'flying_goat' ); ?>" />
 		</div>
 	</form>
